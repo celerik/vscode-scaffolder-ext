a3c28531c7f8b5019ae6884c9f7f93d6e2cd4a3f
Diego Assia G <46972313+DaaGeney@users.noreply.github.com>
2022-06-14 17:59:18 -0500

    Update onSelectMenu.ts

--- source/src/commands/onSelectMenu.ts
+++ source/src/commands/onSelectMenu.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+
 export default vscode.commands.registerCommand('celerik-scaffolder.onOpenMenu', () => {
     const panel = vscode.window.createWebviewPanel(
         'celerikScaffolder', // Identifies the type of the webview. Used internally
