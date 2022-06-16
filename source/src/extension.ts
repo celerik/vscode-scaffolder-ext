7bc0dd6017a949c0a48f5deb2eac9e6ee594f256
Diego <dassia@celerik.com>
2022-06-16 13:01:17 -0500

    feat(boilerplate): Removing unnecesary files

--- source/src/extension.ts
+++ source/src/extension.ts
@@ -1,25 +1,10 @@
 import * as vscode from 'vscode';
 import { ViewLoader } from './view/ViewLoader';
-import { CommonMessage } from './view/messages/messageTypes';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand('webview.open', () => {
+    vscode.commands.registerCommand('celerik-scaffolder.open', () => {
       ViewLoader.showWebview(context);
-    }),
-
-    vscode.commands.registerCommand('extension.sendMessage', () => {
-      vscode.window
-        .showInputBox({
-          prompt: 'Send message to Webview',
-        })
-        .then(result => {
-          result &&
-            ViewLoader.postMessageToWebview<CommonMessage>({
-              type: 'COMMON',
-              payload: result,
-            });
-        });
     })
   );
 }
