59913ff6074b2a5b2812f5f97f97642a2a2a9f08
Carlos Cotera <ccotera@celerik.com>
2022-08-03 17:19:35 -0500

    feat(status bar): adding icon status bar

--- source/src/extension.ts
+++ source/src/extension.ts
@@ -1,12 +1,18 @@
 import * as vscode from 'vscode';
 import { ViewLoader } from './view/ViewLoader';
 
+let myStatusBarItem: vscode.StatusBarItem;
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand('celerik-scaffolder.open', () => {
       ViewLoader.showWebview(context);
     })
   );
+  myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  myStatusBarItem.command = 'celerik-scaffolder.open';
+  context.subscriptions.push(myStatusBarItem);
+  myStatusBarItem.text = 'this is the celerik sca';
+  myStatusBarItem.show();
 }
 
 export function deactivate() { }
